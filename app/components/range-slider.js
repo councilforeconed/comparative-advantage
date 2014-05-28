ef28799ea1e3f06c43084ac681ffdfb6570ea2ca
Steve Kinney <hello@stevekinney.net>
2014-05-28 05:34:47 -0400

    Add fonts; get sliders working

--- app/components/range-slider.js
+++ app/components/range-slider.js
@@ -1,3 +1,9 @@
 export default Ember.Component.extend({
-  classNames: ['clearfix','slider']
+  classNames: ['clearfix','slider'],
+
+  didInsertElement: function() {
+    this.$('input[type=range]').slider();
+  }
+
+
 });
